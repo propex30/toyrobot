d2ea46e1c2e8ef77fae0c31e8552c0aa560066d5
markgrainger <mgrainger1@gmail.com>
2023-07-03 15:03:38 +1000

    check for bad PLACE command

--- src/controller/calculateMoves.ts
+++ src/controller/calculateMoves.ts
@@ -1,10 +1,49 @@
-export class CalculateMoves{
+enum Direction {
+	NORTH = 'NORTH',
+	EAST = 'EAST',
+	SOUTH = 'SOUTH',
+	WEST = 'WEST'
+}
 
-	processMove(args: string){
-		console.log(args);
-		// switch (args.trim()){
-		// case: 'PLACE' console.log();
-		return 'test';
+export class CalculateMoves {
+
+	processMove(args: string) {
+		let response: string = '';
+		const argsArray = args.split(' ');
+		let placeArray: string[];
+
+
+		switch (argsArray[0]) {
+		case 'PLACE':
+			placeArray = argsArray[1].split(',');
+			// if not an allowed placement
+			if (!this.checkIfAllowedPlacement(+placeArray[0], +placeArray[1], placeArray[2] as Direction)) {
+				break;
+			} else {
+				return 'OUTPUT';
+				// TODO move
+			}
+
+
+			break;
+		case 'MOVE':
+			break;
+		case 'LEFT':
+			break;
+		case 'RIGHT':
+			break;
+		case 'REPORT':
+			break;
+		default:
+			break;
+
+		}
+		return response;
+	}
+
+	checkIfAllowedPlacement(x: number, y: number, direction: Direction): boolean {
+		const validDirections = Object.values(Direction);
+		return x >= 0 && x < 5 && y >= 0 && y < 5 && validDirections.includes(direction);
 
 	}
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,7 @@
 import { processQuery } from '@src/controller/processQuery';
 import { CalculateMoves } from '@src/controller/calculateMoves';
 
-jest.mock('@src/controller/calculateMoves');
+//jest.mock('@src/controller/calculateMoves');
 
 
 
@@ -11,145 +11,80 @@ describe('test the robot query responses', () => {
 	afterEach(() => {
 		jest.resetModules();
 	});
-	it('should call processMove and a response "test"', () => {
+	// it('should call processMove and a response "test"', () => {
+	//
+	// 	const testArgs   = 'MOVE';
+	//
+	// 	const result = processQuery(calc, testArgs);
+	//
+	// 	expect(result).toBe('test');
+	// });
 
-		// Arrange
-		const processMoveMock = jest.fn();
-		(CalculateMoves as jest.Mock).mockImplementation(() => {
-			return {processMove: processMoveMock};
-		});
-		const testArgs   = 'MOVE';
+	it('should handle bad PLACE Command by ignoring it and returning empty string', () => {
+
+		const testArgs  = 'PLACE 5,3,NORTH';
 
 		// Act
 		const result = processQuery(calc, testArgs);
 
 		// Assert
-		expect(processMoveMock).toHaveBeenCalledWith(testArgs);
-		expect(result).toBe('test');
+		expect(result).toBe('');
 	});
 
 	it('should handle PLACE Command', () => {
-		// Arrange
-		const processMoveMock = jest.fn();
-		(CalculateMoves as jest.Mock).mockImplementation(() => {
-			return {processMove: processMoveMock};
-		});
 		const testArgs  = 'PLACE 2,3,NORTH';
-
-		// Act
 		const result = processQuery(calc, testArgs);
-
-		// Assert
-		expect(processMoveMock).toHaveBeenCalledWith(testArgs);
-		expect(result).toBe('test');
+		expect(result).toBe('OUTPUT');
 	});
 
 	it('should handle Move Command', () => {
-		// Arrange
-		const processMoveMock = jest.fn();
-		(CalculateMoves as jest.Mock).mockImplementation(() => {
-			return {processMove: processMoveMock};
-		});
+
 		const testArgs  = 'MOVE';
-
-		// Act
 		const result = processQuery(calc, testArgs);
-
-		// Assert
-		expect(processMoveMock).toHaveBeenCalledWith(testArgs);
 		expect(result).toBe('test');
 	});
 
 	it('should handle LEFT Command', () => {
-		// Arrange
-		const processMoveMock = jest.fn();
-		(CalculateMoves as jest.Mock).mockImplementation(() => {
-			return {processMove: processMoveMock};
-		});
 		const testArgs  = 'LEFT';
-
-		// Act
 		const result = processQuery(calc, testArgs);
-
-		// Assert
-		expect(processMoveMock).toHaveBeenCalledWith(testArgs);
 		expect(result).toBe('test');
 	});
 
 	it('should handle RIGHT Command', () => {
-		// Arrange
-		const processMoveMock = jest.fn();
-		(CalculateMoves as jest.Mock).mockImplementation(() => {
-			return {processMove: processMoveMock};
-		});
 		const testArgs  = 'RIGHT';
-
-		// Act
 		const result = processQuery(calc, testArgs);
-
-		// Assert
-		expect(processMoveMock).toHaveBeenCalledWith(testArgs);
 		expect(result).toBe('test');
 	});
 
 	it('should handle REPORT Command', () => {
-		// Arrange
-		const processMoveMock = jest.fn();
-		(CalculateMoves as jest.Mock).mockImplementation(() => {
-			return {processMove: processMoveMock};
-		});
 		const testArgs  = 'REPORT';
-
-		// Act
 		const result = processQuery(calc, testArgs);
-
-		// Assert
-		expect(processMoveMock).toHaveBeenCalledWith(testArgs);
 		expect(result).toBe('test');
 	});
 
 	it('should handle a series of commands and not create a new instance of the class each time', () => {
-		// Arrange
-		const processMoveMock = jest.fn();
-		(CalculateMoves as jest.Mock).mockImplementation(() => {
-			return {processMove: processMoveMock};
-		});
 		const testArgs  = 'REPORT';
-
-		// Act
 		const result = processQuery(calc, testArgs);
-
-		// Assert
-		expect(processMoveMock).toHaveBeenCalledWith(testArgs);
 		expect(result).toBe('test');
 	});
 
 	it('should ignore commands that make it fall off the table', () => {
-		// Arrange
-		const processMoveMock = jest.fn();
-		(CalculateMoves as jest.Mock).mockImplementation(() => {
-			return {processMove: processMoveMock};
-		});
 		const testArgs  = 'PLACE 4,4,NORTH';
 
-		// Act
 		processQuery(calc, testArgs);
 
 		const testArgs2  = 'REPORT';
 
-		// Act
 		const result = processQuery(calc, testArgs2);
 
 		expect(result).toBe('4,4,NORTH');
 
 		const testArgs3  = 'MOVE';
 
-		// Act
 		processQuery(calc, testArgs3);
 
 		const testArgs4  = 'REPORT';
 
-		// Act
 		const result2 = processQuery(calc, testArgs4);
 		// result2 is still the same as before as a move north would place the robot off the table
 		expect(result2).toBe('4,4,NORTH')
